039bdd3ea46b06eabcde33dc3920030298bae452
kanhaiya Yadav <kanhaiyakumar25213@email.com>
2026-05-29 17:32:22 +0530

    Fix Vercel backend export

--- server/index.js
+++ server/index.js
@@ -33,9 +33,7 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('MongoDB connected');
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
   })
   .catch((err) => console.log('DB connection error:', err));
+
+module.exports = app;
